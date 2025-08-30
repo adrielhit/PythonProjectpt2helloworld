a7a51372136c8bf49bc04498fa636f975927be7b
adrielhit <adrielirl24@gmail.com>
2025-08-30 16:46:24 -0400

    Lets go

---
+++ VariablesAIR.py
@@ -0,0 +1,10 @@
+print ("the answer to your query is",6*5)
+print ("the answer to your query is",8/3)
+print ("the answer to your query is",8//3)
+print ("the answer to your query is",8%3)
+print ("the answer to your query is",4200000+23090)
+print ("the answer to your query is",4200000-7633)
+
+
+
+
